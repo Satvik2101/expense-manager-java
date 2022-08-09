837ca3bcacd93424d1c179be84961af3ac338f6f
SatvikGupta <satvik.sli@gmail.com>
2022-08-09 17:17:03 +0530

    Update account amount added

--- src/main/java/GUI/ViewAccountsPage.java
+++ src/main/java/GUI/ViewAccountsPage.java
@@ -5,6 +5,8 @@ import Account.AccountsManager;
 import Account.NullAccount;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.event.MouseAdapter;
@@ -51,14 +53,48 @@ public class ViewAccountsPage extends JFrame {
     private void createUIComponents() {
         String[] columns = {"Id","Name","Amount"};
 
-        TableModel model = new DefaultTableModel(columns,0);
+        TableModel model = new DefaultTableModel(columns,0){
+            @Override
+
+            public boolean isCellEditable(int row, int column)
+            {
+                return column == 2;
+            }
+
+            @Override
+            public Class<?> getColumnClass(int idx){
+                return idx==2?Double.class:super.getColumnClass(idx);
+            }
+        };
+//
+//
         accounts = new JTable(model);
+        model.addTableModelListener(e -> {
+            int row = e.getFirstRow();
+            int col = e.getColumn();
+            if (row<0 || col<0)return;
+            System.out.println(row+" "+col);
+            UUID id = (UUID) model.getValueAt(row,0);
+            double newAmount = (double) model.getValueAt(row,col);
+
+            if (mgr.updateAccountValue(id,newAmount)){
+                JOptionPane.showMessageDialog(this,"Updated Amount");
+
+            }else{
+                JOptionPane.showMessageDialog(this,"");
+            }
+        });
+
+
         accounts.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent me) {
                 if (me.getClickCount() == 2) {     // to detect doble click events
                     JTable target = (JTable)me.getSource();
                     int row = target.rowAtPoint(me.getPoint());
-                    if (row==-1) System.out.println("Negative");
+                    if (row==-1) {
+                        System.out.println("Negative");
+                        return;
+                    }
                     System.out.println(target.getValueAt(row,0));
                     UUID id =(UUID) target.getValueAt(row,0);
                     ViewTransactionsPage viewTransactionsPage = new ViewTransactionsPage(mgr,id);
